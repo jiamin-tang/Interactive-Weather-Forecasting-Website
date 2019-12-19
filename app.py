113ad58b8866f8ab8405278d555b874d6185ae87
yiweisang97 <yiwei_sang@brown.edu>
2019-12-19 15:39:06 +0000

    emmm

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import numpy as np
 import plotly.graph_objects as go
 import dash_table
 import pandas as pd
+
 from data_acquire import load_forecast_data
 from database import fetch_forecast_data_as_df
 
@@ -14,9 +15,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', '/assets/s
 
 # Define the dash app first
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
 # Define component functions
-
-
 def page_header():
     """
     Returns the page header as a dash `html.Div`
@@ -61,19 +61,13 @@ def description():
         Weather data is gathered from 1st July, 2008 to present time and keeps updating.
         ''', className='eleven columns', style={'paddingLeft': '5%'})], className="row")
 
-
-def extract_weekday(date_time):
-    dict_weekday = {0:'Monday', 1:'Tuesday', 2:'Wednesday', 3:'Thursday', 4:'Friday', 5:'Saturday', 6:'Sunday'}
-    return dict_weekday[date_time.weekday()]
-
 def display_date(date_time):
-    return date_time.strftime(format='%Y-%m-%d')
-
+    dict_weekday = {0:'Monday', 1:'Tuesday', 2:'Wednesday', 3:'Thursday', 4:'Friday', 5:'Saturday', 6:'Sunday'}
+    return '{}, {}'.format(date_time.strftime(format='%Y-%m-%d'), dict_weekday[date_time.weekday()])
 
 #df_table = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/solar.csv') #Change city when available
 def weather_table():
     df_static_daily_forecast = fetch_forecast_data_as_df()[0]
-    df_static_daily_forecast['weekday'] = df_static_daily_forecast['datetime'].apply(extract_weekday)
     df_static_daily_forecast['datetime'] = df_static_daily_forecast['datetime'].apply(display_date)
     return html.Div(children=[
         dcc.Markdown('''New York Weather Forecast''', className='row',style={'paddingLeft': '30%'}),
@@ -89,67 +83,24 @@ def weather_table():
         )
     ],style={'marginTop': '2rem', 'width': '500px', 'marginLeft': '200px', 'display': 'inline-block'})
 
-def weather_table_interactive():
-    return html.Div(children=[
-        dcc.Markdown('''Weather information of related days''', className='row',style={'paddingLeft': '30%'}),
-        dash_table.DataTable(
-            id='table_interactive',
-            columns=[{"name": i, "id": i} for i in df_table.columns],
-            data=df_table.to_dict('records'),
-            style_header={'backgroundColor': 'rgb(30, 30, 30)'},
-            style_cell={
-                'backgroundColor': 'rgb(50, 50, 50)',
-                'color': 'white',
-            },
-        )
-    ],style={'marginTop': '2rem', 'width': '500px', 'marginLeft': '200px', 'display': 'inline-block'})
 
-
-df = pd.read_csv('uscities.csv')
-all_states = sorted(df['state_id'].unique())
-all_options = {state:[city for city in df[df['state_id'] == state]['city']] for state in all_states}
-def select_city():
-    """Select the state and city the user wants to enquire"""
-    return html.Div(children=[
-        html.Div(children=[
-        html.Div(children=[
-        dcc.Markdown('''Select a state'''),
-        dcc.Dropdown(
-            id='states-dropdown',
-            options=[{'label': k, 'value': k} for k in all_options.keys()],
-            value='',
-            multi=False,
-            style={'height': '30px', 'width': '300px'}
-        )], style={'width': '300px', 'marginLeft': '90px', 'display': 'inline-block'}),
-
-        html.Div(children=[
-        dcc.Markdown('''Select a city'''),
-        dcc.Dropdown(
-            id='cities-dropdown',
-            style={'height': '30px', 'width': '300px'}
-        )], style={'width': '300px', 'align': 'right', 'marginLeft': '400px', 'display': 'inline-block'})
-        ])])
-
-
-def daily_static_stacked_trend_graph(stack=False):
+def hourly_static_stacked_trend_graph(stack=False):
     """
     Returns scatter line plot of all power sources and power load.
     If `stack` is `True`, the 4 power sources are stacked together to show the overall power
     production.
     """
-    df_day, df_hourly = fetch_historical_data_as_df()
-    if df_day is None:
-        print('df_day None')
+    df_static_hourly_forecast = fetch_forecast_data_as_df()[1]
+    if df_static_hourly_forecast is None:
+        print('Data Cannot be retrieved')
         return go.Figure()
-    sources = ['avgtempC', 'avgtempF', 'sunHour', 'uvIndex']
-    x = df_day['datetime']
+    sources = ['tempC', 'tempF', 'precipMM', 'uvIndex']
+    x = df_static_hourly_forecast['datetime']
     fig = go.Figure()
     for i, s in enumerate(sources):
-        fig.add_trace(go.Scatter(x=x, y=df_day[s], mode='lines', name=s,
+        fig.add_trace(go.Scatter(x=x, y=df_static_hourly_forecast[s], mode='lines', name=s,
                                  line={'width': 2, 'color': COLORS[i]}))
-    #fig.add_trace(go.Scatter(x=x, y=df['Load'], mode='lines', name='Load',
-    #                        line={'width': 2, 'color': 'orange'}))
-    title = 'Daily Weather Visualization'
+    title = '24-Hour New York Weather Forecast'
     if stack:
         title += ' [Stacked]'
     fig.update_layout(template='plotly_dark',
@@ -161,52 +112,74 @@ def daily_static_stacked_trend_graph(stack=False):
     return fig
 
 
-def what_if_description():
-    """
-    Returns description of "What-If" - the interactive component
-    """
+cols = ['datetime', 'sunrise', 'sunset', 'moonrise', 'moonset', 'moon_phase',
+                            'moon_illumination', 'tempC', 'tempF', 'sunHour', 'uvIndex']
+empty_data = {col:['-----']*7 for col in cols}
+df_initial_table = pd.DataFrame(data=empty_data)
+
+def weather_table_interactive():
     return html.Div(children=[
-        dcc.Markdown('''
-        # " What If "
-        So far, BPA has been relying on hydro power to balance the demand and supply of power. 
-        Could our city survive an outage of hydro power and use up-scaled wind power as an
-        alternative? Find below **what would happen with 2.5x wind power and no hydro power at 
-        all**.   
-        Feel free to try out more combinations with the sliders. For the clarity of demo code,
-        only two sliders are included here. A fully-functioning What-If tool should support
-        playing with other interesting aspects of the problem (e.g. instability of load).
-        ''', className='eleven columns', style={'paddingLeft': '5%'})
-    ], className="row")
+        dcc.Markdown('''Weather Forecast of Selected City''', className='row',style={'paddingLeft': '30%'}),
+        dash_table.DataTable(
+            id='table_interactive',
+            columns=[{"name": i, "id": i} for i in df_initial_table.columns],
+            data=df_initial_table.to_dict('records'),
+            style_header={'backgroundColor': 'rgb(30, 30, 30)'},
+            style_cell={
+                'backgroundColor': 'rgb(50, 50, 50)',
+                'color': 'white',
+            },
+        )
+    ],style={'marginTop': '2rem', 'width': '500px', 'marginLeft': '200px', 'display': 'inline-block'})
 
 
+df_interactive_hourly_forecast = None
+
+def interative_hourly_weather_forecast_visulization():
+    if df_interactive_hourly_forecast is None:
+        return go.Figure()
+    sources = ['tempC', 'tempF', 'precipMM', 'uvIndex']
+    x = df_interactive_hourly_forecast['datetime']
+    fig = go.Figure()
+    for i, s in enumerate(sources):
+        fig.add_trace(go.Scatter(x=x, y=df_interactive_hourly_forecast[s], mode='lines', name=s,
+                                 line={'width': 2, 'color': COLORS[i]}))
+    title = '24-Hour Weather Forecast of selected city'
+    if stack:
+        title += ' [Stacked]'
+    fig.update_layout(template='plotly_dark',
+                      title=title,
+                      plot_bgcolor='#23272c',
+                      paper_bgcolor='#23272c',
+                      yaxis_title='',
+                      xaxis_title='Date')
+    return fig
 
 
-
-def what_if_tool():
-    """
-    Returns the What-If tool as a dash `html.Div`. The view is a 8:3 division between
-    demand-supply plot and rescale sliders.
-    """
+df = pd.read_csv('uscities.csv')
+all_state_name = sorted(df['state_name'].unique())
+all_options = {state:[city for city in df[df['state_name'] == state]['city']] for state in all_state_name}
+def select_city():
+    """Select the state and city the user wants to enquire"""
     return html.Div(children=[
-        html.Div(children=[dcc.Graph(id='what-if-figure')], className='nine columns'),
+        html.Div(children=[
+        html.Div(children=[
+        dcc.Markdown('''Select a state'''),
+        dcc.Dropdown(
+            id='states-dropdown',
+            options=[{'label': k, 'value': k} for k in all_options.keys()],
+            value='Alaska',
+            multi=False,
+            style={'height': '30px', 'width': '300px'}
+        )], style={'width': '300px', 'marginLeft': '90px', 'display': 'inline-block'}),
 
         html.Div(children=[
-            html.H5("Rescale Power Supply", style={'marginTop': '2rem'}),
-            html.Div(children=[
-                dcc.Slider(id='wind-scale-slider', min=0, max=4, step=0.1, value=2.5, className='row',
-                           marks={x: str(x) for x in np.arange(0, 4.1, 1)})
-            ], style={'marginTop': '5rem'}),
-
-            html.Div(id='wind-scale-text', style={'marginTop': '1rem'}),
-
-            html.Div(children=[
-                dcc.Slider(id='hydro-scale-slider', min=0, max=4, step=0.1, value=0,
-                           className='row', marks={x: str(x) for x in np.arange(0, 4.1, 1)})
-            ], style={'marginTop': '3rem'}),
-            html.Div(id='hydro-scale-text', style={'marginTop': '1rem'}),
-        ], className='three columns', style={'marginLeft': 5, 'marginTop': '10%'}),
-    ], className='row eleven columns')
-
+        dcc.Markdown('''Select a city'''),
+        dcc.Dropdown(
+            id='cities-dropdown',
+            style={'height': '30px', 'width': '300px'}
+        )], style={'width': '300px', 'align': 'right', 'marginLeft': '400px', 'display': 'inline-block'})
+        ])])
 
 def architecture_summary():
     """
@@ -241,6 +214,7 @@ def dynamic_layout():
         html.Hr(),
         description(),
         weather_table(),
+        hourly_static_stacked_trend_graph(),
         select_city(),
         weather_table_interactive(),
         # dcc.Graph(id='trend-graph', figure=static_stacked_trend_graph(stack=False)),
@@ -270,8 +244,8 @@ def set_cities_value(available_options):
     dash.dependencies.Output('table_interactive', 'data'),
     [dash.dependencies.Input('cities-dropdown', "value")])
 def update_table(city):
-    df_table_interactive, df_hourly_forecast = load_forecast_data(city)
-    return df_table_interactive.to_dict('records')
+    df_interactive_daily_forecast, df_interactive_hourly_forecast = load_forecast_data(city)
+    return df_interactive_hourly_forecast.to_dict('records')
 
 if __name__ == '__main__':
     app.run_server(debug=True, port=1050, host='0.0.0.0')
